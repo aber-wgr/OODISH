584ab63fb824472d17ec58782ccdc5876c4ecca0
Will Robinson <wgr@aber.ac.uk>
2021-02-10 14:02:28 +0000

    retrofitted a previous better-working model and added grid search for more params

--- model.py
+++ model.py
@@ -21,11 +21,11 @@ class ExtensibleEncoder(nn.Module):
             ('dropout1', nn.Dropout2d(self.dropout_chance / 4,True)),
             ('relu1', nn.ReLU()),
             ('maxpool1', nn.MaxPool2d(kernel_size=2, stride=2)), # x/2 x y/2 x conv
-            ('conv2', nn.Conv2d(in_channels=self.conv_scale, out_channels=self.conv_scale**2, kernel_size=3, stride=1, padding=1)),
+            ('conv2', nn.Conv2d(in_channels=self.conv_scale, out_channels=self.conv_scale * 2, kernel_size=3, stride=1, padding=1)),
             ('dropout2', nn.Dropout2d(self.dropout_chance / 2,True)),
             ('relu2', nn.ReLU()),
             ('maxpool2', nn.MaxPool2d(kernel_size=2, stride=2)), # x/4 x y/4 x conv^2
-            ('conv3', nn.Conv2d(in_channels=self.conv_scale**2, out_channels=self.conv_scale**3, kernel_size=3, stride=1, padding=1)),
+            ('conv3', nn.Conv2d(in_channels=self.conv_scale  * 2, out_channels=self.conv_scale * 4, kernel_size=3, stride=1, padding=1)),
             ('dropout3', nn.Dropout2d(self.dropout_chance,True)),
             ('relu3', nn.ReLU()),
             ('maxpool3', nn.MaxPool2d(kernel_size=2, stride=2)), # x/8 x y/8 x conv^3
@@ -44,7 +44,7 @@ class ExtensibleEncoder(nn.Module):
     def rebuild_fc_layers(self, scale): 
         x = scale[1]
         y = scale[0]
-        flattened_size = int((x/8)*(y/8)*(self.conv_scale**3))        
+        flattened_size = int((x/8)*(y/8)*(self.conv_scale * 4))        
         self.fc1 = nn.Linear(in_features=flattened_size,out_features=self.code_size)
                                      
     def forward(self, features):
@@ -61,11 +61,11 @@ class ExtensibleDecoder(nn.Module):
         self.dropout_chance = kwargs["dropout_chance"]
         
         decoderPlanBase = OrderedDict([
-            ('conv3', nn.Conv2d(in_channels=self.conv_scale**3, out_channels=self.conv_scale**2, kernel_size=3, stride=1, padding=1)),
+            ('conv3', nn.Conv2d(in_channels=self.conv_scale * 4, out_channels=self.conv_scale * 2, kernel_size=3, stride=1, padding=1)),
             ('dropout3', nn.Dropout2d(self.dropout_chance,True)),
             ('relu3', nn.ReLU()),
             ('upsample3', nn.Upsample(scale_factor=2,mode='bilinear')),
-            ('conv2', nn.Conv2d(in_channels=self.conv_scale**2, out_channels=self.conv_scale, kernel_size=3, stride=1, padding=1)),
+            ('conv2', nn.Conv2d(in_channels=self.conv_scale*2, out_channels=self.conv_scale, kernel_size=3, stride=1, padding=1)),
             ('dropout2', nn.Dropout2d(self.dropout_chance / 2,True)),
             ('relu2', nn.ReLU()),
             ('upsample2', nn.Upsample(scale_factor=2,mode='bilinear')),
@@ -87,10 +87,10 @@ class ExtensibleDecoder(nn.Module):
     def rebuild_fc_layers(self, scale):
         x = scale[1]
         y = scale[0]
-        flattened_size = int((x/8)*(y/8)*(self.conv_scale**3))
+        flattened_size = int((x/8)*(y/8)*(self.conv_scale * 4))
         
         self.fc1 = nn.Linear(in_features=self.code_size,out_features=flattened_size)
-        self.unflatten = nn.Unflatten(1,(int(self.conv_scale**3),int(x/8),int(y/8)))
+        self.unflatten = nn.Unflatten(1,(int(self.conv_scale*4),int(x/8),int(y/8)))
                                      
     def forward(self, features):
         fc_out = self.fc1(features)
