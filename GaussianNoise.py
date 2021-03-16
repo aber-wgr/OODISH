551472f8aa717b0a856296113ef92a1bafbb7705
Will Robinson <wgr@aber.ac.uk>
2021-03-16 23:23:30 +0000

    Fixed adversarial generation bugs

---
+++ GaussianNoise.py
@@ -0,0 +1,18 @@
+#taken from https://discuss.pytorch.org/t/how-to-add-noise-to-mnist-dataset-when-using-pytorch/59745/2
+#clipped to rescale to stated min/max
+
+import torch
+
+class AddGaussianNoiseAndRescale(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+        
+    def __call__(self, tensor):
+        noisy = tensor + torch.randn(tensor.size()) * self.std + self.mean
+        noisy = noisy - torch.min(noisy)
+        noisy = noisy / torch.max(noisy)
+        return noisy
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
