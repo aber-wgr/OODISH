d52d4f31c82f76da035c87fa2548a047f5e4dd27
Will Robinson <wgr@aber.ac.uk>
2021-03-17 03:51:04 +0000

    Fixes and Mahalanobis distance metric

--- GaussianNoise.py
+++ GaussianNoise.py
@@ -9,10 +9,23 @@ class AddGaussianNoiseAndRescale(object):
         self.mean = mean
         
     def __call__(self, tensor):
-        noisy = tensor + torch.randn(tensor.size()) * self.std + self.mean
+        
+        noise = torch.randn(tensor.size()) * self.std + self.mean
+        noise = noise / torch.max(noise)
+        noisy = tensor + noise
+        
         noisy = noisy - torch.min(noisy)
         noisy = noisy / torch.max(noisy)
         return noisy
     
     def __repr__(self):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+    
+class Rescale(object):
+    def __init__(self):
+        mean = 0.5
+        
+    def __call__(self, tensor):
+        tensor = tensor - torch.min(tensor)
+        tensor = tensor / torch.max(tensor)
+        return tensor
